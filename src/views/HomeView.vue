0c0e804d85086b8bef76a2682a91c8f8132aa13a
RoudroMehedi <roudro.m@gmail.com>
2023-09-07 12:46:22 +0200

    fixed build failure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,7 +18,7 @@
 
     <img
       class="relative block mx-auto mt-5 -mb-20 w-auto max-w-full"
-      src="assets/img/introduction-music.png"
+      src="../../public/assets/img/introduction-music.png"
     />
   </section>
 
